8bee8fc70d5902a33291a5b9d10ad3995a72ca07
Antoine Crenshaw <Antoine_Crenshaw@insideranken.org>
2024-10-12 10:38:18 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,14 @@
-if(process.env.NODE_ENV !== 'production') {
-  import('dotenv').then((dotenv) => dotenv.config());
-}
-
 import express from 'express';
-import dotenv from 'dotenv';
-dotenv.config();
 import debug from 'debug';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import expressLayouts from 'express-ejs-layouts';
+import indexRoutes from './routes/index.js';
+
+dotenv.config();
+
 const debugServer = debug('app:server');
 const app = express();
-import expressLayouts from 'express-ejs-layouts';
-
-import indexRoutes from './routes/index.js';
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -23,7 +21,6 @@ app.set('layout', 'layouts/layout');
 app.use(expressLayouts);
 app.use(express.static('public'));
 
-import mongoose from 'mongoose';
 mongoose.connect(process.env.DATABASE_URL);
 const db = mongoose.connection;
 db.on('error', (error) => console.error(error));
