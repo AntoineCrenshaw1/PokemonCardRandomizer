0b059c55977cd4e5a1092bf89893c0a57ba38a47
Antoine Crenshaw <Antoine_Crenshaw@insideranken.org>
2024-10-12 11:11:20 -0500

    Added database.js

---
+++ database.js
@@ -0,0 +1,31 @@
+import { MongoClient, ObjectId } from 'mongodb';
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+import debug from 'debug';
+const debugDb = debug('app:Database');
+
+/** Generate/Parse an ObjectId */
+const newId = (str) => new ObjectId(str);
+
+/** Global variable storing the open connection, do not use it directly */
+let _db = null;
+
+async function connect() {
+  if(!_db){
+   const dbUrl = process.env.DB_URL;
+   const dbName = process.env.DB_NAME;
+   const client = await MongoClient.connect(dbUrl);
+   _db = client.db(dbName);
+  }
+  return _db;
+}
+
+/** Connect to the database and verify the connection */
+async function ping(){
+  const db = await connect();
+  const pong = await db.command({ ping: 1 });
+  debugDb(`Ping: ${JSON.stringify(pong)}`);
+}
+
+ping();
