58de33845c8c68efea2bfbb4999c1e9be190195e
Antoine_Crenshaw <Antoine_Crenshaw@insideranken.org>
2024-10-13 16:02:55 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 import express from 'express';
 import debug from 'debug';
 import dotenv from 'dotenv';
-import expressLayouts from 'express-ejs-layouts';
 import { indexRoutes } from './routes/index.js';
 
 dotenv.config();
@@ -11,20 +10,13 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static('frontend/dist'))
 
 app.use('/', indexRoutes);
 
 app.set('view engine', 'ejs');
 app.set('views', './views');
-app.set('layout', 'layouts/layout');
-app.use(expressLayouts);
 app.use(express.static('public'));
-
-// Error handling middleware
-app.use((err, req, res, next) => {
-  debugServer(`Error: ${err.message}`);
-  res.status(500).json({ message: 'Internal Server Error', error: err.message });
-});
 
 const port = process.env.PORT || 3000;
 
