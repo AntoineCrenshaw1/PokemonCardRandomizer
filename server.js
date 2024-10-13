b938c4fa010af82badb41497e320fe88d41df513
Antoine Crenshaw <Antoine_Crenshaw@insideranken.org>
2024-10-12 21:11:18 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 import express from 'express';
 import debug from 'debug';
 import dotenv from 'dotenv';
-import expressLayouts from 'express-ejs-layouts';
 import { indexRoutes } from './routes/index.js';
 
 dotenv.config();
@@ -14,11 +13,8 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use('/', indexRoutes);
 
-app.set('view engine', 'ejs');
-app.set('views', './views');
-app.set('layout', 'layouts/layout');
-app.use(expressLayouts);
-app.use(express.static('public'));
+
+app.use(express.static('frontend/dist'));
 
 // Error handling middleware
 app.use((err, req, res, next) => {
