9eb92205a63dff73bf2716985ae8cf656b812653
Antoine_Crenshaw <Antoine_Crenshaw@insideranken.org>
2024-10-20 20:01:35 -0500

    Update files

--- frontend/app/components/OpenPack.tsx
+++ frontend/app/components/OpenPack.tsx
@@ -1,12 +1,12 @@
-'use client'
-import React from 'react'
+"use client";
+import React from "react";
 
 const OpenPack = () => {
   return (
     <div>
-       <button onClick={() => console.log('Pack opened')}>Open Pack</button>
+      <button onClick={() => console.log("Pack opened")}>Open Pack</button>
     </div>
-  )
-}
+  );
+};
 
-export default OpenPack
+export default OpenPack;
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,15 +1,11 @@
 import Link from "next/link";
-import OpenPack from "./components/OpenPack";
 
 export default function Home() {
   return (
     <>
-    <main>
-    <h1>Home</h1>
-    <Link href="/cards">Cards</Link>
-    <OpenPack />
-    <Link href="/pages">Pack Page</Link>
-    </main>
+      <main>
+        <Link href="/cards">Cards</Link>
+      </main>
     </>
   );
 }
